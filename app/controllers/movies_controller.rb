a8b0b328f02229bdd24aab1e088ae0f8e256b519
Militsa Neykova <militsaneykovany@gmail.com>
2017-09-11 08:47:43 -0400

    add movie controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,57 @@
+class MoviesController < ApplicationController
+        
+            before_action :one_movie, only: [:show, :edit, :update, :destroy]    
+        
+            def index
+                @movies = Movie.all    
+            end
+        
+            def show
+            
+            end
+        
+            def new
+                @movie = Movie.new   
+                @songs = Song.all 
+            end
+        
+            def create
+                @movie = Movie.new(movie_params)
+                @songs = @movie.songs
+                if @movie.save
+                    redirect_to '/' 
+                else 
+                    render 'new' 
+                end
+            end
+        
+            def edit 
+                
+            end
+        
+            def update
+                if @movie.update(movie_params)
+                    redirect_to movie_path(@movie)
+                else
+                    render 'edit'   
+                end    
+            end
+        
+            def destroy
+                if @movie.destroy
+                    redirect_to '/'
+                  else
+                    render 'edit'
+                  end
+            end
+        
+            private
+                def movie_params
+                    params.require(:movie).permit(:name, :description, :director, :song_id)
+                end
+        
+                def one_movie
+                    @movie = Movie.find(params[:id])
+                end
+        end
+
